e88ca42a84917c2f4fe64c8180dd652f03408402
Hussain101 <hussainsidd88@gmail.com>
2023-10-03 11:29:50 +0500

    middleware complete

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextURL } from "next/dist/server/web/next-url";
+import { NextResponse,NextRequest } from "next/server";
+
+export function middleware(request : NextRequest){
+    console.log('====================================');
+    console.log(request);
+    console.log('====================================');
+
+    const path = request.nextUrl.pathname
+    const token = request.cookies.get('token')?.value || "";
+    const ispublicPath = path === '/login' || path === "/signup";
+
+    if (ispublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+
+    }
+    if (!ispublicPath && !token) {
+        return NextResponse.redirect(new URL("/login",request.nextUrl))
+    }
+}
+
+//matching paths
+
+export const config = {
+    matcher:[
+        '/',
+        '/profile',
+        '/profile/:path*',
+        '/login',
+        '/signup',
+    ]
+}
