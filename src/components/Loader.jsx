6e97a20ef8e5ccfbf18761ab6da63c66d2c9da39
Hussain101 <hussainsidd88@gmail.com>
2023-11-13 23:46:28 +0500

    loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='bg-black h-screen flex justify-center items-center'>
+    <div className='bg-black h-screen  flex justify-center items-center'>
         
         <div className="honeycomb">
   <div></div>
-  <div></div>
+  <div>Loading</div>
   <div></div>
   <div></div>
   <div></div>
